c9745f6da30e74620780335f749b2a6e46ccbcce
Frank Zeng <frankhzeng@gmail.com>
2026-09-16 16:02:18 -0400

    update to 2.2 and fix build issue

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -30,6 +30,7 @@
 #include "main.h"
 #include "FreeRTOS.h"
 #include "cmsis_os2.h"
+#include "task.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
